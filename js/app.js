42e912149d9e14763b99c5cb38302a97daf67c31
David Kolia <Dkolia@icloud.com>
2018-06-19 18:50:34 -0500

    Set up filestructure, added code instructions, added some Pseudocode

---
+++ js/app.js
@@ -0,0 +1,97 @@
+console.log("Homework3");
+// Palindrome
+// Write a function checkPalindrome that accepts a single argument, a string. The function should return true (Boolean) if the string is a palindrome, false if it is not. Make sure your function will give the correct answer for words with capital letters.
+
+get word
+remove caps
+break word into letters
+push letters into array
+duplicate array
+reverse array
+compare X-Many letters of each array
+if all match, return true
+else return false
+
+
+console.log(checkPalindrome("Radar"));
+ => true
+console.log(checkPalindrome("Borscht"));
+=> false
+
+// 🔴 **Commit your work.**
+// The commit message should read:
+// "Commit 2 - Palindrome".
+
+
+
+// Digit Sum
+// Write a function sumDigits that accepts a number and returns the sum of its digits.
+
+// establish sumDigits function
+// ask for a number: userNumber
+// userNumber.length
+// push to array
+// add numbers together
+// consolelog sum
+//
+// console.log(sumDigits(42));
+// => 6;
+
+// 🔴 **Commit your work.**
+// The commit message should read:
+// "Commit 3 - Digit Sum".
+
+
+
+// Pythagoras
+// Write a function calculateSide that takes two arguments: sideA and sideB, and returns the solution for sideC using the Pythagorean theorem.
+//
+// hint: discover the Pythagorean Theorem on a website called google.com
+// hint: checkout the Math methods in javascript
+
+// console.log(calculateSide(8, 6));
+// => 10
+// 🔴 **Commit your work.**
+// The commit message should read:
+// "Commit 4 - Pythagoras".
+
+
+
+// Sum Array
+// Write a function sumArray that takes an array as an argument. The array should contain numbers. The function should return the sum of the numbers in the array. Use a for loop within the function to iterate over the array and sum the contents. Use a variable such as
+//
+// let sum = 0;
+// that will accumulate value within the loop. Expected result:
+
+// console.log(sumArray([1, 2, 3, 4, 5, 6]));
+// => 21
+// 🔴 **Commit your work.**
+// The commit message should read:
+// "Commit 5 - Sum Array".
+
+
+
+// Prime Numbers
+// A Prime number is a number that is not evenly divisible by another number except 1 and itself. If you want to read more deeply about it, go here. To test whether a number is Prime, you only need to test as far as the square root of that number. This is advisable for optimization and testing large numbers.
+
+// Step One
+// Write a function called checkPrime that will test whether a number is Prime. The function will return true (Boolean) if Prime, false if not. Hint: Check every number up to the square root. To do this, try a for loop.
+
+// Step Two
+// Write another function called printPrimes that will print (console log) all the Primes up to an arbitrary limit. For example, if you invoke your function with printPrimes(97), it will print all the Prime numbers up to and including 97. This function can call on the previous checkPrime function.
+
+// 🔴 **Commit your work.**
+// The commit message should read:
+// "Commit 6 - Prime Numbers".
+
+
+
+// Insert Dash
+// Write a function insertDash that accepts a number as a parameter and returns a string with a dash inserted between any consecutive odd numbers. There should not be a dash at the end, it goes only between numbers.
+
+// console.log(insertDash(454793));
+// => 4547-9-3
+
+// 🔴 **Commit your work.**
+// The commit message should read:
+// "Commit 7 - Insert Dash".
